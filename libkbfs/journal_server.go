c0b0ca47311e6a04dd4669b17d9527c52e3b789f
Frederick Akalin <akalin@keyba.se>
2016-07-07 22:45:54 -0700

    Fix panic

--- libkbfs/journal_server.go
+++ libkbfs/journal_server.go
@@ -206,7 +206,7 @@ func (j journalMDOps) put(ctx context.Context, rmd *RootMetadata, journal *mdSer
 
 func (j journalMDOps) Put(ctx context.Context, rmd *RootMetadata) error {
 	if rmd.MergedStatus() == Unmerged {
-		panic(fmt.Errorf("bad md = %+v", rmd))
+		fmt.Printf("bad md = %+v\n", rmd)
 		return UnexpectedUnmergedPutError{}
 	}
 
